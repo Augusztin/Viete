6b7368e6f13c9b97f24974a8f247d8b92521c496
Augusztin <augusztinn@kkszki.hu>
2025-11-20 19:26:51 +0100

    complete

---
+++ viete.js
@@ -0,0 +1,19 @@
+document.getElementById("button1").onclick = () => {
+    var a = document.getElementById("inputa").value;
+    var b = document.getElementById("inputb").value;
+    var c = document.getElementById("inputc").value;
+    var D = Math.sqrt(b*b-4*a*c)
+    var eredmeny = "";
+
+
+    if (D<0){
+        eredmeny = "Nincs megoldás.";
+    }
+    else if (D==0){
+        eredemeny = "Egy megoldása van.";
+    }
+    else {
+        eredmeny = "Két megoldása van.";
+    }
+    document.getElementById("span1").innerHTML = eredmeny;
+}
